cb82c360f76f5ce6e9e8e104cb87054eee7abdd3
MorhafGhziel <GhzielMorhaf@gmail.com>
2025-02-27 16:23:08 +0300

    fix: make logo ticker fully responsive

--- components/LogoTicker.js
+++ components/LogoTicker.js
@@ -6,7 +6,7 @@ import { logos } from "@/constants";
 
 const LogoTicker = () => {
   return (
-    <div className="bg-white w-full h-[120px] flex items-center justify-center">
+    <div className="bg-white w-full h-[60px] sm:h-[80px] md:h-[100px] lg:h-[100px] flex items-center justify-center">
       <div className="overflow-hidden w-full">
         <motion.div
           className="flex items-center"
@@ -24,14 +24,14 @@ const LogoTicker = () => {
           {logos.map((logo, i) => (
             <div
               key={`logo-1-${i}`}
-              className="shrink-0 h-[100px] flex items-center justify-center"
+              className="shrink-0 h-[50px] sm:h-[70px] md:h-[90px] lg:h-[100px] flex items-center justify-center"
             >
               <Image
                 src={logo}
                 alt={`Partner Logo ${i + 1}`}
                 width={980}
                 height={100}
-                className="object-contain w-auto h-[100px]"
+                className="object-contain w-auto h-[40px] sm:h-[60px] md:h-[80px] lg:h-[100px]"
                 priority={i === 0}
               />
             </div>
@@ -40,14 +40,14 @@ const LogoTicker = () => {
           {logos.map((logo, i) => (
             <div
               key={`logo-2-${i}`}
-              className="shrink-0 h-[100px] flex items-center justify-center"
+              className="shrink-0 h-[50px] sm:h-[70px] md:h-[90px] lg:h-[100px] flex items-center justify-center"
             >
               <Image
                 src={logo}
                 alt={`Partner Logo ${i + 1}`}
                 width={980}
                 height={100}
-                className="object-contain w-auto h-[100px]"
+                className="object-contain w-auto h-[40px] sm:h-[60px] md:h-[80px] lg:h-[100px]"
               />
             </div>
           ))}
@@ -55,14 +55,14 @@ const LogoTicker = () => {
           {logos.map((logo, i) => (
             <div
               key={`logo-3-${i}`}
-              className="shrink-0 h-[100px] flex items-center justify-center"
+              className="shrink-0 h-[50px] sm:h-[70px] md:h-[90px] lg:h-[100px] flex items-center justify-center"
             >
               <Image
                 src={logo}
                 alt={`Partner Logo ${i + 1}`}
                 width={980}
                 height={100}
-                className="object-contain w-auto h-[100px]"
+                className="object-contain w-auto h-[40px] sm:h-[60px] md:h-[80px] lg:h-[100px]"
               />
             </div>
           ))}
